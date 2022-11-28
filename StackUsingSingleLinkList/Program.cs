50eb2b490e09f322fe1d7e4180f7d21dd2c1d188
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-11-28 17:29:19 +0800

    create bool empty and if else function

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -20,6 +20,12 @@ namespace StackUsingSingleLinkList
         {
             top = null;
         }
-        
+        bool empty()
+        {
+            if (top == null)
+                return (true);
+            else
+                return (false);
+        }
     }
 }
