0f60ed830f854a44c7378b93cc812da423f6c84e
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-11-28 17:55:21 +0800

    create object and menu

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -51,12 +51,27 @@ namespace StackUsingSingleLinkList
                 Console.WriteLine("\nStack Empty");
             else
             {
-                for (tmp= top; tmp != null; tmp = tmp.next)
+                for(tmp = top; tmp != null; tmp = tmp.next)
                 {
-                    Console.WriteLine();
-                    Console.WriteLine("\n***Stack Menu\n");
+                    Console.WriteLine(tmp.info);
                 }
             }
+        }
+
+        static void Main(string[] args)
+        {
+            stack s = new stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push ");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine(" Display ");
+                Console.WriteLine("4. Exit ");
+                Console.WriteLine("\nEnter Your Choice: ");
+            }
+            
         }
     }
 }
