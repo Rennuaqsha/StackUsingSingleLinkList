06e320e1d6df59a7c6418ae0f984183225a65b80
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-11-28 17:24:13 +0800

    create namespace,class node and variable

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+namespace StackUsingSingleLinkList
+{
+    class node
+    {
+        public int info;
+        public node next;
+        public node(int i, node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
+}
